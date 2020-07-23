59d3868c3b4ab871fc57186f7c74df66aaa6f4d3
99nk <58465142+99nk@users.noreply.github.com>
2020-07-24 01:23:37 +0530

    Update users.js
    .

--- users.js
+++ users.js
@@ -1,45 +1,36 @@
-// Import Dependencies
-const url = require('url')
-const MongoClient = require('mongodb').MongoClient
+const {MongoClient} = require('mongodb');
+async function main()
+{
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://untanglegeek:Geeks%2320Ism%2ANerds@makaero.qc4nx.mongodb.net/makearo?retryWrites=true&w=majority";
+ 
 
-// Create cached connection variable
-let cachedDb = null
-
-// A function for connecting to MongoDB,
-// taking a single parameter of the connection string
-async function connectToDatabase(uri) {
-  // If the database connection is cached,
-  // use it instead of creating a new connection
-  if (cachedDb) {
-    return cachedDb
-  }
-
-  // If no connection is cached, create a new one
-  const client = await MongoClient.connect(uri, { useNewUrlParser: true })
-
-  // Select the database through the connection,
-  // using the database path of the connection string
-  const db = await client.db(url.parse(uri).pathname.substr(1))
-
-  // Cache the database connection and return the connection
-  cachedDb = db
-  return db
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+module.exports = (req, res) => {
+ res.send(`Hello`)
 }
 
-// The main, exported, function of the endpoint,
-// dealing with the request and subsequent response
-module.exports = async (req, res) => {
-  // Get a database connection, cached or otherwise,
-  // using the connection string environment variable as the argument
-  const db = await connectToDatabase(process.env.MONGODB_URI)
-
-  // Select the "users" collection from the database
-  const collection = await db.collection('users')
-
-  // Select the users collection from the database
-  const users = await collection.find({}).toArray()
-
-  // Respond with a JSON string of all users in the collection
-  res.status(200).json({ users })
-  res.status(200).send("Working")
-}
+main().catch(console.error);
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+ //   console.log("Databases:");	
+  //  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
